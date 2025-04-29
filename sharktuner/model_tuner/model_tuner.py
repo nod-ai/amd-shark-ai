nod-ai/amd-shark-ai

commit 966ac1f36a21960b37b87ed596fae7ea7c736243

[Tuner] Renaming to sharktuner (#1297)
Renaming tuner as part of #1248

---------

Signed-off-by: Muzammiluddin Syed <muzasyed@amd.com>

sharktuner/model_tuner/model_tuner.py
@@ -6,11 +6,11 @@
 
 import argparse
 from pathlib import Path
-from tuner import libtuner
-from tuner.common import *
+from sharktuner import libtuner
+from sharktuner.common import *
 
 
-class SimpleTuner(libtuner.TuningClient):
+class ModelTuner(libtuner.TuningClient):
     def __init__(self, tuner_context: libtuner.TunerContext):
         super().__init__(tuner_context)
         self.compile_flags: list[str] = []
@@ -42,30 +42,30 @@ def read_flags_file(flags_file: str) -> list[str]:
 def arg_parse() -> argparse.Namespace:
     # Custom arguments for the example tuner file.
     parser = argparse.ArgumentParser(description="Autotune sample script")
-    client_args = parser.add_argument_group("Simple Example Tuner Options")
+    client_args = parser.add_argument_group("Shark Tuner Options")
     client_args.add_argument(
-        "simple_model_file", type=Path, help="Path to the model file to tune (.mlir)"
+        "model_file", type=Path, help="Path to the model file to tune (.mlir)"
     )
     client_args.add_argument(
-        "--simple-num-dispatch-candidates",
+        "--num-dispatch-candidates",
         type=int,
         default=None,
         help="Number of dispatch candidates to keep for model benchmarks.",
     )
     client_args.add_argument(
-        "--simple-num-model-candidates",
+        "--num-model-candidates",
         type=int,
         default=None,
         help="Number of model candidates to produce after tuning.",
     )
     client_args.add_argument(
-        "--simple-compile-flags-file",
+        "--compile-flags-file",
         type=str,
         default="",
         help="Path to the flags file for iree-compile.",
     )
     client_args.add_argument(
-        "--simple-model-benchmark-flags-file",
+        "--model-benchmark-flags-file",
         type=str,
         default="",
         help="Path to the flags file for iree-benchmark-module for model benchmarking.",
@@ -93,10 +93,8 @@ def main() -> None:
         libtuner.validate_devices(args.devices)
         print("Validation successful!\n")
 
-    compile_flags: list[str] = read_flags_file(args.simple_compile_flags_file)
-    model_benchmark_flags: list[str] = read_flags_file(
-        args.simple_model_benchmark_flags_file
-    )
+    compile_flags: list[str] = read_flags_file(args.compile_flags_file)
+    model_benchmark_flags: list[str] = read_flags_file(args.model_benchmark_flags_file)
 
     summary_log_file = path_config.base_dir / "summary.log"
     summary_handler = logging.FileHandler(summary_log_file)
@@ -107,20 +105,20 @@ def main() -> None:
     print("Generating candidate tuning specs...")
     with TunerContext(logger=root_logger) as tuner_context:
         tuner_context.logger.addHandler(summary_handler)
-        simple_tuner = SimpleTuner(tuner_context)
+        model_tuner = ModelTuner(tuner_context)
         candidates = libtuner.generate_candidate_specs(
-            args, path_config, candidate_trackers, simple_tuner
+            args, path_config, candidate_trackers, model_tuner
         )
         print(f"Stored candidate tuning specs in {path_config.specs_dir}\n")
         if stop_after_phase == libtuner.ExecutionPhases.generate_candidates:
             return
 
         print("Compiling dispatch candidates...")
-        simple_tuner.compile_flags = compile_flags + [
+        model_tuner.compile_flags = compile_flags + [
             "--compile-from=executable-sources"
         ]
         compiled_candidates = libtuner.compile(
-            args, path_config, candidates, candidate_trackers, simple_tuner
+            args, path_config, candidates, candidate_trackers, model_tuner
         )
         if stop_after_phase == libtuner.ExecutionPhases.compile_dispatches:
             return
@@ -128,13 +126,13 @@ def main() -> None:
         message = "Benchmarking compiled dispatch candidates..."
         print(message)
         logging.info(message)
-        simple_tuner.benchmark_flags = ["--input=1", "--benchmark_repetitions=3"]
+        model_tuner.benchmark_flags = ["--input=1", "--benchmark_repetitions=3"]
         top_candidates = libtuner.benchmark(
             args,
             compiled_candidates,
             candidate_trackers,
-            simple_tuner,
-            args.simple_num_dispatch_candidates,
+            model_tuner,
+            args.num_dispatch_candidates,
         )
         logging.info(f"Top dispatch candidates: {top_candidates}")
         for id in top_candidates:
@@ -143,15 +141,15 @@ def main() -> None:
             return
 
         print("Compiling models with top candidates...")
-        simple_tuner.compile_flags = compile_flags
-        simple_tuner.compile_timeout = 120
+        model_tuner.compile_flags = compile_flags
+        model_tuner.compile_timeout = 120
         compiled_model_candidates = libtuner.compile(
             args,
             path_config,
             top_candidates,
             candidate_trackers,
-            simple_tuner,
-            args.simple_model_file,
+            model_tuner,
+            args.model_file,
         )
         if stop_after_phase == libtuner.ExecutionPhases.compile_models:
             return
@@ -159,14 +157,14 @@ def main() -> None:
         message = "Benchmarking compiled model candidates..."
         print(message)
         logging.info(message)
-        simple_tuner.benchmark_flags = model_benchmark_flags
-        simple_tuner.benchmark_timeout = 60
+        model_tuner.benchmark_flags = model_benchmark_flags
+        model_tuner.benchmark_timeout = 60
         top_model_candidates = libtuner.benchmark(
             args,
             compiled_model_candidates,
             candidate_trackers,
-            simple_tuner,
-            args.simple_num_model_candidates,
+            model_tuner,
+            args.num_model_candidates,
         )
         logging.info(f"Top model candidates: {top_model_candidates}")
         for id in top_model_candidates:

.github/workflows/ci-sharktuner.yml
@@ -4,20 +4,20 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-name: CI - Tuner
+name: CI - sharktuner
 
 on:
   workflow_dispatch:
   pull_request:
     paths:
-      - '.github/workflows/ci-tuner.yml'
-      - 'tuner/**'
+      - '.github/workflows/ci-sharktuner.yml'
+      - 'sharktuner/**'
   push:
     branches:
       - main
     paths:
-      - '.github/workflows/ci-tuner.yml'
-      - 'tuner/**'
+      - '.github/workflows/ci-sharktuner.yml'
+      - 'sharktuner/**'
 
 concurrency:
   group: ${{ github.workflow }}-${{ github.event.number || github.sha }}
@@ -41,15 +41,15 @@ jobs:
       - name: Install dev dependencies
         run: |
           python -m pip install --upgrade pip
-          pip install -r tuner/requirements-dev.txt
+          pip install -r sharktuner/requirements-dev.txt
 
-      - name: Install tuner dependencies
+      - name: Install sharktuner dependencies
         run: |
-          pip install -r tuner/requirements-tuner.txt
+          pip install -r sharktuner/requirements-tuner.txt
           pip install -r requirements-iree-unpinned.txt
 
-      - name: Run tuner tests
-        run: pytest tuner/
+      - name: Run sharktuner tests
+        run: pytest sharktuner/
 
       - name: Run mypy type checker
-        run: mypy tuner/tuner
+        run: mypy sharktuner/sharktuner

sharktuner/model_tuner/__main__.py
@@ -4,6 +4,6 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-from . import simple_tuner
+from . import model_tuner
 
-simple_tuner.main()
+model_tuner.main()

commit 86b1487a4807f278fefcec9e12c30a3b3d7945e9

[sharktuner] resolving option name conflict with libtuner (#1328)
Fixing runtime error in sharktuner introduced by
https://github.com/nod-ai/shark-ai/commit/966ac1f36a21960b37b87ed596fae7ea7c736243

---------

Signed-off-by: Muzammiluddin Syed <muzasyed@amd.com>

sharktuner/model_tuner/model_tuner.py
@@ -47,13 +47,13 @@ def arg_parse() -> argparse.Namespace:
         "model_file", type=Path, help="Path to the model file to tune (.mlir)"
     )
     client_args.add_argument(
-        "--num-dispatch-candidates",
+        "--model-tuner-num-dispatch-candidates",
         type=int,
         default=None,
         help="Number of dispatch candidates to keep for model benchmarks.",
     )
     client_args.add_argument(
-        "--num-model-candidates",
+        "--model-tuner-num-model-candidates",
         type=int,
         default=None,
         help="Number of model candidates to produce after tuning.",
@@ -132,7 +132,7 @@ def main() -> None:
             compiled_candidates,
             candidate_trackers,
             model_tuner,
-            args.num_dispatch_candidates,
+            args.model_tuner_num_dispatch_candidates,
         )
         logging.info(f"Top dispatch candidates: {top_candidates}")
         for id in top_candidates:
@@ -164,7 +164,7 @@ def main() -> None:
             compiled_model_candidates,
             candidate_trackers,
             model_tuner,
-            args.num_model_candidates,
+            args.model_tuner_num_model_candidates,
         )
         logging.info(f"Top model candidates: {top_model_candidates}")
         for id in top_model_candidates:

sharktuner/sharktuner/libtuner.py
@@ -279,12 +279,6 @@ def parse_arguments(
         help="Stop execution after specified phase",
     )
     general_args.add_argument(
-        "--num-model-candidates",
-        help="Maximum number of stage 2 candidates",
-        type=int,
-        default=50,
-    )
-    general_args.add_argument(
         "--dry-run",
         action="store_true",
         help="Do not attempt to run any modules or initialize the IREE runtime",