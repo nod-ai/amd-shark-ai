32de67b8ed2deb00ccee5971e782782119eabc6b
Amily Wu <amilywu2@amd.com>
2025-12-09 12:43:46 -0800

    Remove unused pytest lib

--- amdsharktuner/tests/process_utils_test.py
+++ amdsharktuner/tests/process_utils_test.py
@@ -4,8 +4,6 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-import pytest
-
 from amdsharktuner import process_utils
 
 
