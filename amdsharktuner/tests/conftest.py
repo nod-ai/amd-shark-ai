7db83384f76885b41bb07ac0e9a6b6595942b844
Amily Wu <amilywu2@amd.com>
2026-01-07 14:00:44 -0800

    Add license to tests

--- amdsharktuner/tests/conftest.py
+++ amdsharktuner/tests/conftest.py
@@ -1,4 +1,10 @@
-from __future__ import annotations
+# Copyright 2025 Advanced Micro Devices, Inc.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+
 from typing import Any
 
 from amdsharktuner.test_utils import configure_pytest_multiprocessing
