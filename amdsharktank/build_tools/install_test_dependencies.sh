0d6e9aa26a8c809478cb4e69c6449db4077daafe
Yash Rathore <yrathore@amd.com>
2026-01-14 16:29:45 +0530

    Update install_test_dependencies.sh

--- amdsharktank/build_tools/install_test_dependencies.sh
+++ amdsharktank/build_tools/install_test_dependencies.sh
@@ -57,8 +57,9 @@ else
     pip install --no-compile -r "$SRC_DIR/requirements-iree-pinned.txt"
     pip uninstall -y wave-lang
     pip install wave-lang
-    pip uninstall -y iree-base-compiler iree-base-runtime
-    pip install   --find-links https://iree.dev/pip-release-links.html   iree-base-compiler==3.10.0rc20260113   iree-base-runtime==3.10.0rc20260113  iree-turbine==3.10.0rc20260113
+    pip install --find-links https://iree.dev/pip-release-links.html iree-turbine==3.10.0rc20260113
+    pip uninstall -y iree-base-compiler iree-base-runtime 
+    pip install --find-links https://iree.dev/pip-release-links.html iree-base-compiler==3.10.0rc20260113   iree-base-runtime==3.10.0rc20260113
     # pip install -f https://github.com/iree-org/wave/releases/expanded_assets/dev-wheels wave-lang --no-index
 fi
 
