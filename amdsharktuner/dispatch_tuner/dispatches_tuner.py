9983f06ff20481109d6ffb388ef4c82c578be979
Amily Wu <amilywu2@amd.com>
2026-01-16 00:20:58 +0000

    Update dispatches tuner

--- amdsharktuner/dispatch_tuner/dispatches_tuner.py
+++ amdsharktuner/dispatch_tuner/dispatches_tuner.py
@@ -6,9 +6,11 @@ import time
 from datetime import datetime
 import logging
 import shutil
+import sys
 
 random.seed(42) 
 redo_list = [
+
 ]
 ok_list = [
 
@@ -16,8 +18,6 @@ ok_list = [
 failed_list = [
 
 ]
-PICK_SAMPLE = False
-MAX_SAMPLE_SIZE = 5
 
 DEVICE="hip://0,hip://1,hip://2,hip://3,hip://4,hip://5,hip://6,hip://7"
 TUNING_TASKS=["llvmgpu_vector_distribute", "llvmgpu_tile_and_fuse"]
@@ -34,7 +34,7 @@ def setup_logging() -> logging.Logger:
 
     # Create file handler for logging to a file.
     # file_handler = logging.FileHandler(run_log_path, mode="w")
-    file_handler = logging.FileHandler(run_log_path, mode="w")
+    file_handler = logging.FileHandler(run_log_path)
     file_handler.setLevel(logging.DEBUG)
 
     # Create stream handler for logging to the console (only warnings and higher).
@@ -65,26 +65,32 @@ def setup_logging() -> logging.Logger:
     return logging.getLogger()
 
 
-def main():
-    logger = setup_logging()
-    mlir_folder_path = Path("~/iree-kernel-benchmark/dump_dispatch/problem_mlir_dump").expanduser().resolve()
-    logger.debug(f"In MLIR folder {mlir_folder_path}: ")
-    mlir_files = sorted(mlir_folder_path.glob("*.mlir"))
-    for f in mlir_files:
-        logger.debug(f"{f.stem}")
+def append_line(path: Path, line: str) -> None:
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with path.open("a", encoding="utf-8") as f:
+        f.write(line.rstrip("\n") + "\n")
 
+
+def main():
+    if len(sys.argv) < 2:
+        raise SystemExit("Usage: python dispatches_tuner.py <arch>\nExample: python dispatches_tuner.py gfx942")
+    arch = sys.argv[1]
+    
+    logger = setup_logging()
     base_path = Path(os.path.dirname(os.path.abspath(__file__)))
+    success_list_path = base_path / "success_list.log"
+    failed_list_path = base_path / "failed_list.log"
+    append_line(success_list_path, f"{arch}")
+    append_line(failed_list_path, f"{arch}")
+    
+    logger.debug(f"Arch: {arch}")
     mlir_benchmark_folder_path = (base_path / "dump").expanduser().resolve()
     logger.debug(f"In MLIR_benchmark folder {mlir_benchmark_folder_path}: ")
-    mlir_benchmark_files = sorted(mlir_benchmark_folder_path.glob("*.mlir"))
+    mlir_benchmark_files = sorted(mlir_benchmark_folder_path.glob("*.bench"))
     for f in mlir_benchmark_files:
         logger.debug(f"{f.stem}")
 
-    logger.info(f"Found {len(mlir_files)} mlir file(s)")
     logger.info(f"Found {len(mlir_benchmark_files)} benchmark file(s)")
-
-    if len(mlir_files) != len(mlir_benchmark_files):
-        logger.warning("Mismatch between number of MLIR files and benchmark files!")
 
     failed_files = []
     ok = fail = 0
@@ -103,7 +109,7 @@ def main():
     logger.info(f"Tuning Vars: {var_list}")
 
     for i, bench in enumerate(mlir_benchmark_files, start=1):
-        mlir_filename = bench.stem.replace("_benchmark","")
+        mlir_filename = bench.stem
         logger.info(f"Checking file {i} / {len(mlir_benchmark_files)}")
 
         # Check list
@@ -113,25 +119,24 @@ def main():
         if mlir_filename in failed_list:
             logger.debug(f"Skipping file {mlir_filename} in failed list")
             continue
-        mlir = Path(f"{mlir_folder_path}/{mlir_filename}.mlir")
 
         # Check file
-        if not mlir.exists():
-            logger.warning(f"Can't find {mlir}, skipping")
+        if not bench.exists():
+            logger.warning(f"Can't find {bench}, skipping")
             fail += 1
-            failed_files.append(mlir.name)
+            failed_files.append(bench.name)
             continue
 
         # logger.info("=" * 80)
         # logger.info("=" * 80)
         tuning_tasks = TUNING_TASKS
         for j, codegen_pipeline in enumerate(tuning_tasks, start=1):
-            logger.info(f"Tuning {i} ({j}/{len(tuning_tasks)}) / {len(mlir_benchmark_files)}: {mlir.name} - {codegen_pipeline}")
+            logger.info(f"Tuning {i} ({j}/{len(tuning_tasks)}) / {len(mlir_benchmark_files)}: {bench.name} - {codegen_pipeline}")
             file_start = time.perf_counter()
             logger.debug(f"File {bench} started at {start_dt.isoformat(timespec='seconds')}")
             cmd = [
                 "python3", "-m", "dispatch_tuner",
-                str(mlir),
+                str(bench),
                 str(bench),
                 "--compile-flags-file=dispatch_tuner/compile_flags.txt",
                 f"--devices={DEVICE}",
@@ -153,13 +158,18 @@ def main():
             # Handle result
             if rc == 0:
                 ok += 1
-                logger.info(f"{finished_at.isoformat(timespec='seconds')} - {mlir.name}: completed in {elapsed:.2f}")
+                elapsed_min = elapsed / 60.0
+                logger.info(
+                    f"{finished_at.isoformat(timespec='seconds')} - "
+                    f"{bench.name}: completed in {elapsed_min:.2f} min"
+                )
+                append_line(success_list_path, f"{bench.name} - {codegen_pipeline} - {elapsed_min:.2f} mins")
                 if elapsed < 60:
                     time.sleep(60 - elapsed) # Make sure next tuning folder is a new folder
 
                 # Move CSV to tuning database
                 folders = sorted(
-                    base_path.parent.glob("tuning_2025_*"),
+                    base_path.parent.glob("tuning_2026_*"),
                     key=lambda p: p.stat().st_mtime,
                     reverse=True
                 )
@@ -172,17 +182,20 @@ def main():
                         dst_dir = csv_dir_vd
                     elif codegen_pipeline == "llvmgpu_tile_and_fuse":
                         dst_dir = csv_dir_tf
-                    shutil.copy2(csv_file, dst_dir)
-                    logging.debug(f"Copied {csv_file.name} -> {dst_dir}")
+                    try:
+                        shutil.copy2(csv_file, dst_dir)
+                        logging.debug(f"Copied {csv_file} -> {dst_dir}")
+                    except:
+                        logging.error(f"Fail to copy {csv_file} -> {dst_dir}")
             else:
                 fail += 1
-                failed_files.append(f"{mlir.name} - {codegen_pipeline}")
-                logger.warning(f"{finished_at.isoformat(timespec='seconds')} - {mlir.name}: 'FAIL({rc})' in {elapsed:.2f}")
-
+                failed_files.append(f"{bench.name} - {codegen_pipeline}")
+                append_line(success_list_path, f"{bench.name} - {codegen_pipeline}")
+                logger.warning(f"{finished_at.isoformat(timespec='seconds')} - {bench.name}: 'FAIL({rc})' in {elapsed:.2f}")
 
     # --- summary logging ---
     if failed_files:
-        logger.warning(f"Failed MLIR files {len(failed_files)}):")
+        logger.warning(f"Failed bench files {len(failed_files)}):")
         for name in failed_files:
             logger.warning(f"- {name}")
 
